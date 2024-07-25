b0d49c6132404d0829fcaa415313d5691b4fd92e
johnwgomez <johnwgomez10@gmail.com>
2024-07-25 16:48:26 -0700

    saving notes so far. working on js notes

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -1,25 +1,30 @@
+// event listener for submit after blog form completed| https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
 document.getElementById('blog-form').addEventListener('submit', function (event) {
   event.preventDefault();
 
+  // values for form collection|https://developer.mozilla.org/en-US/docs/Web/API/Document/getElementById
   const username = document.getElementById('username').value;
   const title = document.getElementById('title').value;
   const content = document.getElementById('content').value;
 
+  //empty field |https://stackoverflow.com/questions/9056112/javascript-shorthand-if-else-and-returning
   if (!username || !title || !content) {
-      alert('Please fill out all fields.');
-      return;
+    alert('Please fill out all fields.');
+    return;
   }
-
+  // grouping for object json parse 
   const blogPost = {
-      username,
-      title,
-      content,
+    username,
+    title,
+    content,
   };
-
+  // retrieve existing blog posts from localStorage |https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
   let blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+  // add new post to array |https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
   blogPosts.push(blogPost);
+  // store updated array |https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
   localStorage.setItem('blogPosts', JSON.stringify(blogPosts));
-
+  // redirect user to next page html to view | https://developer.mozilla.org/en-US/docs/Web/API/Location/href
   window.location.href = 'blog.html';
 });
 
--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,6 +1,7 @@
+// Add an event listener to the theme toggle button
 document.getElementById('theme-toggle').addEventListener('click', function () {
   document.body.classList.toggle('dark-mode');
-  
+  // dark them if/else 
   if (document.body.classList.contains('dark-mode')) {
     this.textContent = '🌙';
   } else {
