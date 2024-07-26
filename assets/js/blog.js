64661a10b4b55639fd27044b1b292afe28ce0e2c
johnwgomez <johnwgomez10@gmail.com>
2024-07-26 07:54:52 -0700

    had issues with remote repo but now fixed, everything noted as well

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,17 +1,23 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event |event listener for the page to load
 window.addEventListener('DOMContentLoaded', (event) => {
+    // Get the HTML element with the ID 'blog-posts' and store
   const blogPostsContainer = document.getElementById('blog-posts');
-
+//check if element exists
   if (blogPostsContainer) {
+    // retrieval from localStorage
       const blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
-
+// loop
       blogPosts.forEach(post => {
+        //create new content as div to represent a submit blog post
           const postEl = document.createElement('div');
           postEl.classList.add('post');
+          //add post and set HTML to display accordingly |https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
           postEl.innerHTML = `
               <h2>${post.title}</h2>
               <p>${post.content}</p>
               <p>Posted by: ${post.username}</p>
           `;
+          //append newly created div to the container
           blogPostsContainer.appendChild(postEl);
       });
   }
